0351a05f76c36dd6061bb180f79a674b42cebe6c
yasuhito1105 <yatti10@gmail.com>
2024-12-05 17:36:30 +0900

    パスの設定　空の状態では登録できない設定　ログイン時の表示の変更　アクションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :email, :password, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date, presence: true
 end
