34d2362d9f04465e045a763cdcf432e4a725c926
yasuhito1105 <yatti10@gmail.com>
2024-12-06 12:27:12 +0900

    バリデーションとパスの追加　ログインできるようになった

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :email, presence: true
+  validates :password, presence: true
   validates :nickname, presence: true
   validates :last_name, presence: true
   validates :first_name, presence: true
