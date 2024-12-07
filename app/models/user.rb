b14418a3ae1284d5f2bbb03333699210225e0eb6
yasuhito1105 <yatti10@gmail.com>
2024-12-07 12:45:34 +0900

    単体テストコードの記述　バリデーション正規表現の記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, presence: true
-  validates :password, presence: true
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d-]+(\.[a-z\d-]+)*\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name, presence: true, format: { with: /\A[^ -~｡-ﾟ]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[^ -~｡-ﾟ]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ア-ン]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ア-ン]+\z/ }
   validates :birthdate, presence: true
 end
