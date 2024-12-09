089dd3bc41dac9be6fb5263b47afdd6011984d79
yasuhito1105 <yatti10@gmail.com>
2024-12-09 15:41:23 +0900

    レビュー後の変更　不要なバリデーション削除、正規表現を一般的なものに変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,25 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, presence: true,
-                    format: {
-                      with: /\A[\w+\-.]+@[a-z\d-]+(\.[a-z\d-]+)*\.[a-z]+\z/i,
-                      message: "Email can't be blank"
-                    },
-                    uniqueness: { case_sensitive: false }
   validates :password, presence: true,
-                       length: { minimum: 6, maximum: 128 },
                        format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/,
                                  message: 'is invalid. Include both letters and numbers' }
   validates :nickname, presence: true
   validates :last_name, presence: true,
                         format: {
-                          with: /\A[^ -~｡-ﾟ]+\z/,
+                          with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,
                           message: 'is invalid. Input full-width characters'
                         }
   validates :first_name, presence: true,
                          format: {
-                           with: /\A[^ -~｡-ﾟ]+\z/,
+                           with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,
                            message: 'is invalid. Input full-width characters'
                          }
   validates :last_name_kana, presence: true,
