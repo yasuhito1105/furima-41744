ad6823b35b38e859c9dd5126d5f3c0d619c6477f
yasuhito1105 <yatti10@gmail.com>
2024-12-09 10:39:37 +0900

    エラーハンドリングの表示の調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,36 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d-]+(\.[a-z\d-]+)*\.[a-z]+\z/i, message: "Email can't be blank" },
-                    uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6, maximum: 128 },
-                       format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers' }
+  # validates :email, presence: true,
+  #                   format: {
+  #                     with: /\A[\w+\-.]+@[a-z\d-]+(\.[a-z\d-]+)*\.[a-z]+\z/i,
+  #                     message: "Email can't be blank"
+  #                   },
+  #                   uniqueness: { case_sensitive: false }
+  # validates :password, presence: true,
+  #                      length: { minimum: 6, maximum: 128 },
+  #                      format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i,
+  #                                message: 'is invalid. Include both letters and numbers' }
   validates :nickname, presence: true
   validates :last_name, presence: true,
-                        format: { with: /\A[^ -~｡-ﾟ]+\z/, message: 'is invalid. Input full-width characters' }
+                        format: {
+                          with: /\A[^ -~｡-ﾟ]+\z/,
+                          message: 'is invalid. Input full-width characters'
+                        }
   validates :first_name, presence: true,
-                         format: { with: /\A[^ -~｡-ﾟ]+\z/, message: 'is invalid. Input full-width characters' }
+                         format: {
+                           with: /\A[^ -~｡-ﾟ]+\z/,
+                           message: 'is invalid. Input full-width characters'
+                         }
   validates :last_name_kana, presence: true,
-                             format: { with: /\A[ア-ン]+\z/, message: 'is invalid. Input full-width katakana characters' }
+                             format: {
+                               with: /\A[ア-ン]+\z/,
+                               message: 'is invalid. Input full-width katakana characters'
+                             }
   validates :first_name_kana, presence: true,
-                              format: { with: /\A[ア-ン]+\z/, message: 'is invalid. Input full-width katakana characters' }
+                              format: {
+                                with: /\A[ア-ン]+\z/,
+                                message: 'is invalid. Input full-width katakana characters'
+                              }
   validates :birthdate, presence: true
 end
