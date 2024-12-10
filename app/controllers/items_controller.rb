cd22c08ca1c153de6affb3e92e345d6e2a622dad
yasuhito1105 <yatti10@gmail.com>
2024-12-10 11:18:48 +0900

    パスの設定　出品画面への遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,17 +5,17 @@ class ItemsController < ApplicationController
   end
 
   def new
-    # @item = Item.new
+    @item = Item.new
   end
 
-  # def create
-  #   @user = User.new(user_params)
-  #   if @user.save
-  #     redirect_to user_path, notice
-  #   else
-  #     render :new, status: :unprocessable_entity
-  #   end
-  # end
+  def create
+    @item = Item.new(user_params)
+    if @item.save
+      redirect_to user_path, notice
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   def destroy
   end
