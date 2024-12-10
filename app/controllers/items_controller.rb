42bdcf9ec2f2305642b57913b74664c055d5a709
yasuhito1105 <yatti10@gmail.com>
2024-12-10 12:09:06 +0900

    アクティブストレージに関する記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,16 @@ class ItemsController < ApplicationController
   def destroy
   end
 
+  private
+
+  def message_params
+    permitted_attributes = [:user, :image, :product_name, :product_description, :category_id, :product_condition_id,
+                            :shipping_fee_id, :shipping_origin_region_id, :price]
+
+    params.require(:item).permit(:user, :image, :product_name, :product_description, :category_id, :product_condition_id,
+                                 :shipping_fee_id, :shipping_origin_region_id, :price).merge(user_id: current_user.id)
+  end
+
   # def move_to_index
   # return if user_signed_in?
 
