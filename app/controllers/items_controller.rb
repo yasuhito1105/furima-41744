3a61090300923ac7b8370d1def51eb52906fdf0c
yasuhito1105 <yatti10@gmail.com>
2024-12-14 14:41:49 +0900

    レビュー後の補足

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,7 +44,6 @@ class ItemsController < ApplicationController
   end
 
   def correct_user
-    @item = Item.find(params[:id])
     return if current_user.id == @item.user.id
 
     redirect_to item_path
